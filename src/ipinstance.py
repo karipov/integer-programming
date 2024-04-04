26c92aa0ea04e9137392f56ffdeb07c8598da0ee
Komron Aripov <komron@brown.edu>
2024-04-04 17:56:42 -0400

    feat: add skeleton!

--- src/ipinstance.py
+++ src/ipinstance.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import numpy  as np
 from docplex.mp.model import Model
+import heapq as pq
 
 @dataclass(frozen=True)
 class IPConfig:
@@ -36,6 +37,14 @@ def data_parse(filename : str) :
         print(f"Error reading instance file. File format may be incorrect.{e}")
         exit(1)
 
+class Node:
+    def __init__(self, lp_objective_value: float, decisions: dict = {}):
+        # floating point value of the LP relaxation
+        self.lp_objective_value = lp_objective_value
+        # decisions made: i -> 1 if test i is used, 0 otherwise
+        self.decisions = decisions
+    
+
 class IPInstance:
     def __init__(self,filename : str) -> None:
         numT,numD,cst,A = data_parse(filename)
@@ -44,6 +53,29 @@ class IPInstance:
         self.costOfTest = cst
         self.A = A
         self.model = Model() #CPLEX solver
+        
+        # add problem constraints: use_vars[i] = 1 if test i is used, 0 otherwise
+        self.use_vars = [self.model.continuous_var(name='use_{0}'.format(i), lb=0, ub=1)
+                         for i in range(self.numTests)]
+
+        # for every pair of diseases, there must be at least one test that can distinguish them
+        for i in range(self.numDiseases):
+            for j in range(self.numDiseases):
+                if (i!= j): 
+                    diff = [np.abs(self.A[k][j] - self.A[k][i]) for k in range(self.numTests)]
+                    diff_x_use = [diff[k] * self.use_vars[k] for k in range(self.numTests)]
+                    self.model.add_constraint(self.model.sum(diff_x_use) >= 1)
+        
+        # minimize the costs of all the used tests
+        costs = [self.use_vars[i] * self.costOfTest[i] for i in range(self.numTests)]
+        self.model.minimize(self.model.sum(costs))
+        
+        # store the incumbent
+        self.incumbent: Node = Node(float('inf'))
+        
+        # initialize the priority queue to the root node
+        self.model.solve_lp()
+        self.priority_queue = pq.heapify([Node(self.model.objective_value)])
   
     def toString(self):
         out = ""
@@ -55,29 +87,51 @@ class IPInstance:
         out += f"A:\n{A_str}"
         
         return out
+
+    def branch(parent: Node):
+        """ Gives us two children nodes by branching on the next variable """
+        
+        # choose variable to branch on
+        
+        # make the children nodes (solve the LP relaxation)
+        
+        # assess the nodes (incumbent, prune, add to queue)
+    
+    def pick_variable(self):
+        """ Heuristics for picking the next variable """
+        pass
   
-    def solve(self):
-        # mvars_i = 1 if test i is used, otherwise 0
-        mvars = [self.model.continuous_var(name='mvar_{0}'.format(i), lb=0, ub=1) for i in range(self.numTests)]
-
-        for i in range(self.numDiseases):
-            for j in range(self.numDiseases):
-                if (i!= j):
-                    # for every pair of diseases
-                    diff = [np.abs(self.A[k][j] - self.A[k][i]) for k in range(self.numTests)]
-                    self.model.add_constraint(self.model.sum(diff[k] * mvars[k] for k in range(self.numTests)) >= 1)
-
-        self.model.minimize(self.model.sum(mvars[i] * self.costOfTest[i] for i in range(self.numTests)))
-
+    def solve_lp(self):
+        """ Solve the LP relaxation """
         sol = self.model.solve()
 
-        obj_value = np.ceil(self.model.objective_value) 
+        obj_value = self.model.objective_value
         if sol:
             self.model.print_information()
        
             print(f"Objective Value: {obj_value}")
         else:
             print("No solution found!")
+
+    def solve_ip(self):
+        """ Branch and bound implementation to solve IP using LP"""
+        
+        while not self.pq:
+            # pick the next node to explore
+            parent_node = pq.heappop(self.pq)
+
+            # check if the node is dominated by the incumbent, if so prune it
+            if parent_node.lp_objective_value >= self.incumbent.lp_objective_value:
+                continue
+            
+            # branch on the node
+            self.branch()
+            
+            
+            
+            
+        
+
 
     
 
---
+++ src/util.py
@@ -0,0 +1,10 @@
+
+def check_integer_solution(solution) -> bool:
+    """
+    Checks if our LP relaxation solution is a solution to the IP problem
+    """
+    threshold = 1e-5
+    thresh_check = lambda x: abs(x - round(x)) > threshold
+    return all(map(thresh_check, solution.as_df()['value']))
+
+
